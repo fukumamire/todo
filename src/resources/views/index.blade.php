14cf934e25daf2260086781a499efe5f8e7a2a4c
fukumamire <rinka_karyou@hotmail.com>
2024-02-09 21:48:01 +0900

    1章　Todoアプリ初級　１－３テーブルの作成　了　

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,12 +4,13 @@
 <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
-@session('content')
+@section('content')
 <div class="todo__alert">
     <div class="todo__alert--success">
     Todoを作成しました
     </div>
 </div>
+
 <div class="todo__content">
   <form class="create-form">
     <div class="create-form__item">
